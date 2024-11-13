de6755cbbf0e8c63e00c9374b72ecf79abad67f9
condillac <44513710+bruhosabto@users.noreply.github.com>
2024-11-13 20:15:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-
 from digitos import Digito
 from random import randint
 
+def main(nome, quantidade):
+    # Abre o arquivo em modo de escrita
+    with open(nome, "a+") as criar:
+        for _ in range(quantidade):
+            # Gera um valor aleatório de peso entre 8 e 16
+            a = randint(8, 16)
+            # Instancia a classe Digito com o peso gerado
+            gere = Digito(a)
+            # Gera a senha e grava no arquivo
+            senha = gere.gerente()
+            criar.write(senha + "\n")
+            print(senha)
 
+# Solicita o nome do arquivo e a quantidade de senhas
+nome = input("Informe o nome para o arquivo de saída: ")
+quantidade = int(input("Informe a quantidade de senhas: "))
 
-
-
-
-def main(nome,quantidade):
-	
-	criar = open(nome,"a+")
-	contador = 0
-	while contador < quantidade:
-		
-		contador += 1
-		a = randint(8,16)
-		gere = Digito(a)
-		
-		criar.write(gere.gerente()+str("\n"))
-		print gere.gerente()+"\n"
-		
-
-
-nome = raw_input("informe o nome para o arquivo de saida:")
-quantidade = input("informe a quantidade senhas:")
-
-main(nome,quantidade)
+main(nome, quantidade)
